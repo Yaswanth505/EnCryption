a1eadb1f8f7bb5263d9567c36810c4049421a339
Yaswanth505 <yaswanthakula02@gmail.com>
2022-05-28 20:11:40 +0530

    Encryption level 4 ==> Bcrypt + salting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 require("dotenv").config();
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
@@ -38,34 +39,46 @@ app.get("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+    
     User.findOne({email: username}, function(err, foundUser){
         if(err)
             console.log(err);
         else
         {
-            if(foundUser.password === password)
-                res.render("secrets");
-            else{
-                console.log("wrong email or Password\nplease check it and try again.");
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result === true)
+                    res.render("secrets");
+                else
+                    console.log("wrong email or Password\nplease check it and try again.");
+            });
+
+
+            
+                
+           
+
+
         }
     });
 })
 
 
 app.post("/register",function(req,res){
-    const newUser = new User(
-    {
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err) 
+                console.log(err);
+            else
+                res.render('secrets');
+        });
     });
-    newUser.save(function(err){
-        if(err) 
-            console.log(err);
-        else
-            res.render('secrets');
-    });
+
+    
 });
 
 app.listen("3000",function(){
